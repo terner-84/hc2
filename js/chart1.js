e15017b7d304f81dd10b7e16eacb93fd97cae6f9
terner-84 <ctiradorama@gmail.com>
2019-06-19 20:54:24 +0200

    init

---
+++ js/chart1.js
@@ -0,0 +1,34 @@
+
+function drawIt() {
+
+    document.getElementById('id01').style.display='block';
+
+    google.charts.load('current', {
+        'packages':['geochart'],
+        // Note: you will need to get a mapsApiKey for your project.
+        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+      });
+      google.charts.setOnLoadCallback(drawRegionsMap);
+    
+      function drawRegionsMap() {
+        var data = google.visualization.arrayToDataTable([
+          ['Země', 'Oblíbenost'],
+          ['Germany', 200],
+          ['United States', 300],
+          ['Brazil', 400],
+          ['Canada', 500],
+          ['France', 600],
+          ['RU', 700]
+        ]);
+    
+        var options = {};
+    
+        var chart = new google.visualization.GeoChart(document.getElementById('hc2Chart'));
+    
+        chart.draw(data, options);
+        
+      }
+
+}
+
---
+++ js/hc2.js
@@ -0,0 +1,62 @@
+$().ready(function(){
+    
+    let window_width = $(window).width() / 2;
+    let adjuster = $('#modal01_content').width() / 2;
+    window_width -= adjuster;
+    $('#modal01_content').css({left: window_width});
+
+    $('#mycheck').click(function(){
+        if ($(this).is(':checked')) {
+            $('#mytext').prop('disabled', true);
+            $('#mytext').prop('placeholder', 'disabled');
+        } else {
+            $('#mytext').prop('disabled', false);
+            $('#mytext').val('0,000');
+        }
+        
+    });
+
+    //draggable modal
+    dragElement(document.getElementById("modal01_content"));
+
+    function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV: 
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+    }
+});
